f96a8187680acdd5e91a2a4aac0de40871e8642d
Max Katz <maxkatz6@outlook.com>
2023-02-16 00:25:33 -0500

    Add CompiledBindingProjectWide repro

---
+++ CompiledBindingProjectWide.axaml.cs
@@ -0,0 +1,18 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace RiderXamlRepros;
+
+public partial class CompiledBindingProjectWide : UserControl
+{
+    public CompiledBindingProjectWide()
+    {
+        InitializeComponent();
+    }
+
+    private void InitializeComponent()
+    {
+        AvaloniaXamlLoader.Load(this);
+    }
+}
