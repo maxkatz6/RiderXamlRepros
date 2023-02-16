9ebd2cf47f16087abf9281e58f9418db1fe7c094
Max Katz <maxkatz6@outlook.com>
2023-02-16 01:05:15 -0500

    Add OptionsAutocompletion repro

---
+++ OptionsAutocompletion.axaml.cs
@@ -0,0 +1,13 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace RiderXamlRepros;
+
+public partial class OptionsAutocompletion : UserControl
+{
+    public OptionsAutocompletion()
+    {
+        InitializeComponent();
+    }
+}
